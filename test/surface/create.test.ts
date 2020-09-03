a0d6fee052ba6a5ff9957bff596207a45919c640
Michiel de Jong <michiel@unhosted.org>
2020-09-03 15:58:33 +0200

    Improve tests for create

--- test/surface/create.test.ts
+++ test/surface/create.test.ts
@@ -6,29 +6,11 @@ const ALICE_WEBID = process.env.ALICE_WEBID;
 
 // jest.setTimeout(30000);
 
-async function createFixture(mode, predicate, authFetcher, testFolderUrl) {
-  const containerUrl = `${testFolderUrl}${predicate}${mode}/`;
-  // This will do mkdir-p:
-  await authFetcher.fetch(`${containerUrl}/test.txt`, {
-    method: 'PUT',
-    body: 'hello'
-  });
-  const aclDocUrl = await findAclDocUrl(containerUrl, authFetcher);
-  await authFetcher.fetch(aclDocUrl, {
-    method: 'PUT',
-    body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:${predicate} <${containerUrl}>;\n  acl:mode acl:${mode}.\n`,
-    headers: {
-      'Content-Type': 'text/turtle'
-    }
-  });
-}
-
 describe('Create', () => {
   let authFetcherAlice;
   let authFetcherBob;
   
   const { testFolderUrl } = generateTestFolder();
-  const containerUrl = `${testFolderUrl}empty/`;
   beforeEach(async () => {
     authFetcherAlice = await getAuthFetcher('alice');
     authFetcherBob = await getAuthFetcher('bob');
@@ -42,50 +24,159 @@ describe('Create', () => {
   });
 
   describe('Using POST', () => {
-    function testFor(mode, predicate, allowed) {
-      it(`Is ${allowed ? 'allowed' : 'not allowed'} with ${predicate} ${mode} access`, async () => {
-        await createFixture(mode, predicate, authFetcherAlice, testFolderUrl);
-        const result = await authFetcherAlice.fetch(`${testFolderUrl}${predicate}${mode}/`, {
-          method: 'POST',
-          body: 'hello'
-        });
-        expect(result.status).toEqual((allowed ? 201 : 403));
+    it(`Is allowed with accessTo Append access`, async () => {
+      const containerUrl = `${testFolderUrl}accessToAppend/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
       });
-    }
-
-    testFor('Read', 'accessTo', false);
-    testFor('Append', 'accessTo', true);
-    testFor('Write', 'accessTo', true);
-    testFor('Read', 'accessTo', false);
-    // testFor('Read', 'default', false); this one fails on NSS, probably an NSS bug?
-    testFor('Append', 'default', false);
-    testFor('Write', 'default', false);
-    testFor('Read', 'default', false);
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Append to Bob:
+        // body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Append.\n`,
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Append, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherAlice.fetch(`${testFolderUrl}accessToAppend/`, {
+        method: 'POST',
+        body: 'hello'
+      });
+      expect(result.status).toEqual(201);
+    });
+    it(`Is allowed with accessTo Write access`, async () => {
+      const containerUrl = `${testFolderUrl}accessToWrite/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
+      });
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Write to Bob:
+        // body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Write.\n`,
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Write, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherBob.fetch(`${testFolderUrl}accessToWrite/`, {
+        method: 'POST',
+        body: 'hello'
+      });
+      expect(result.status).toEqual(201);
+    });
+    it(`Is disallowed otherwise`, async () => {
+      const containerUrl = `${testFolderUrl}allOtherModes/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
+      });
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Write to Bob:
+        // body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Write.\n`,
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#one> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Read, acl:Control.\n<#two> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:default <${containerUrl}>;\n  acl:mode acl:Read, acl:Append, acl:Write, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherAlice.fetch(`${containerUrl}/`, {
+        method: 'POST',
+        body: 'hello'
+      });
+      expect(result.status).toEqual(403);
+    });
   });
 
   describe('Using PUT', () => {
-    function testFor(mode, predicate, allowed) {
-      it(`Is ${allowed ? 'allowed' : 'not allowed'} with ${predicate} ${mode} access`, async () => {
-        await createFixture(mode, predicate, authFetcherAlice, testFolderUrl);
-        const result = await authFetcherBob.fetch(`${testFolderUrl}${predicate}${mode}/new.txt`, {
-          method: 'PUT',
-          body: 'hello',
-          headers: {
-            'Content-Type': 'text/plain',
-            'If-None-Match': '*'
-          }
-        });
-        expect(result.status).toEqual((allowed ? 201 : 403));
+    it(`Is allowed with accessTo and default Write access`, async () => {
+      const containerUrl = `${testFolderUrl}accessToAndDefaultWrite/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
       });
-    }
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Append to Bob:
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:default <${containerUrl}>;\n  acl:mode acl:Write, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherBob.fetch(`${containerUrl}/new.txt`, {
+        method: 'PUT',
+        body: 'hello',
+        headers: {
+          'Content-Type': 'text/plain',
+          'If-None-Match': '*'
+        }
+      });
+      expect(result.status).toEqual(201);
+    });
+    it(`requires default Write`, async () => {
+      const containerUrl = `${testFolderUrl}allOtherModes/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
+      });
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Write to Bob:
+        // body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Write.\n`,
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#one> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Read, acl:Append, acl:Write, acl:Control.\n<#two> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:default <${containerUrl}>;\n  acl:mode acl:Read, acl:Append, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherBob.fetch(`${containerUrl}/new.txt`, {
+        method: 'PUT',
+        body: 'hello',
+        headers: {
+          'Content-Type': 'text/plain',
+          'If-None-Match': '*'
+        }
+      });
+      expect(result.status).toEqual(403);
+    });
 
-    testFor('Read', 'accessTo', false);
-    testFor('Append', 'accessTo', false);
-    testFor('Write', 'accessTo', false);
-    testFor('Read', 'accessTo', false);
-    testFor('Read', 'default', false);
-    testFor('Append', 'default', false);
-    testFor('Write', 'default', true);
-    testFor('Read', 'default', false);
+    it(`requires accessTo Write`, async () => {
+      const containerUrl = `${testFolderUrl}allOtherModes/`;
+      // This will do mkdir-p:
+      await authFetcherAlice.fetch(`${containerUrl}/test.txt`, {
+        method: 'PUT',
+        body: 'hello'
+      });
+      const aclDocUrl = await findAclDocUrl(containerUrl, authFetcherAlice);
+      await authFetcherAlice.fetch(aclDocUrl, {
+        method: 'PUT',
+        // FIXME: leave Alice with Control, but give only Write to Bob:
+        // body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#this> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Write.\n`,
+        body: `@prefix acl: <http://www.w3.org/ns/auth/acl#>.\n\n<#one> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:accessTo <${containerUrl}>;\n  acl:mode acl:Read, acl:Append, acl:Control.\n<#two> a acl:Authorization;\n  acl:agent <${ALICE_WEBID}>;\n  acl:default <${containerUrl}>;\n  acl:mode acl:Read, acl:Append, acl:Write, acl:Control.\n`,
+        headers: {
+          'Content-Type': 'text/turtle'
+        }
+      });
+      const result = await authFetcherBob.fetch(`${containerUrl}/new.txt`, {
+        method: 'PUT',
+        body: 'hello',
+        headers: {
+          'Content-Type': 'text/plain',
+          'If-None-Match': '*'
+        }
+      });
+      expect(result.status).toEqual(403);
+    });
+
   });
 });
