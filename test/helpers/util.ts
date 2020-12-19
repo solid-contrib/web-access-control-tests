cc4ec427b9b95611256b1136c78cb8ab4c717491
Yvo Brevoort <yvo@muze.nl>
2020-12-19 17:29:16 +0100

    Fix error with missing argument
    Fixes this issue: test/helpers/util.ts:33:9 - error TS2794: Expected 1 arguments, but got 0. Did you forget to include 'void' in your type argument to 'Promise'?

--- test/helpers/util.ts
+++ test/helpers/util.ts
@@ -24,7 +24,7 @@ export class WPSClient {
       // console.log('WS <', msg);
       this.received.push(msg);
     });  
-    await new Promise((resolve) => {
+    await new Promise<void>((resolve) => {
       this.ws.on('open', async () => {
         const authHeaders = await getAuthHeaders(this.resourceUrl, 'GET', this.authFetcher);
         await this.send(`sub ${this.resourceUrl}`);
