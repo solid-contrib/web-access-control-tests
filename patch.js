0274f4b7443791846f746e19ec29d11baf697fd4
Michiel de Jong <michiel@unhosted.org>
2021-02-15 13:56:24 +0100

    patch.js and put.js

---
+++ patch.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const { getAuthFetcher, getNodeSolidServerCookie } = require('solid-auth-fetcher');
+
+const oidcIssuer = 'https://solidcommunity.net';
+const nssUsername = 'solidtestsuite';
+const nssPassword = 'Testing123';
+const origin = 'https://tester';
+
+async function run(url) {
+  console.log('Getting cookie', { oidcIssuer, nssUsername, nssPassword });
+  const cookie = await getNodeSolidServerCookie(oidcIssuer, nssUsername, nssPassword);
+  console.log('Getting fetcher', { oidcIssuer, cookie, origin });
+  const fetcher = await getAuthFetcher(oidcIssuer, cookie, origin);
+  console.log('Fetching', { url });
+  const result = await fetcher.fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/sparql-update'
+    },
+    body: 'INSERT DATA { <#patch> <#to> <#create> }'
+  });
+  console.log(result.status, await result.text());
+}
+
+// ...
+run(process.argv[2]);
