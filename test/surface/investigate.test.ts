da33b077a4522bc0d815ee4d8eeaaeed87d22aa3
Michiel de Jong <michiel@unhosted.org>
2021-02-18 14:51:50 +0100

    No longer need this, we can now use node ./fetch.js http://localhost:3000 instead

--- test/surface/investigate.test.ts
+++
@@ -1,7 +0,0 @@
-import { SolidLogic } from 'solid-logic';
-import { generateTestFolder, getSolidLogicInstance, WEBID_ALICE, WEBID_BOB } from '../helpers/env';
-test.skip('investigate', async () => {
-  const solidLogicAlice = await getSolidLogicInstance('ALICE');
-  const result = await solidLogicAlice.fetch(`http://localhost:3000/.acl`);
-  console.log(result.status, await result.text());
-})
