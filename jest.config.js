c2b7e3591ae77e003749cf3be2921b98012c761c
Pete Edwards <edwardsph@users.noreply.github.com>
2021-02-18 11:48:24 +0000

    Increase timeout as some tests failed, perhaps when using remote IdP

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     testEnvironment: 'node',
     verbose: false,
     collectCoverage: false,
+    testTimeout: 10000,
     globals: {
         'ts-jest': {
             // reference: https://kulshekhar.github.io/ts-jest/user/config/
